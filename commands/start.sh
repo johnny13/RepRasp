6469297f66ecc423ef071eccfd93c16e555a8a0d
Derek <derek@huement.com>
2015-07-28 23:06:09 -0500

    oops

--- commands/start.sh
+++ commands/start.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 unclutter &
 matchbox-window-manager &
-chromium-browser --kiosk octopi.local:9001 &
-cd /home/pi/RepRasp && /usr/local/bin/grunt connect:server
-
+chromium-browser --kiosk octopi.local:9001
